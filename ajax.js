9919b04829a2b853903787efdc8e38393988dcbb
Sh1zome <shizomech@gmail.com>
2022-05-05 18:52:01 +0500

    get docs

---
+++ ajax.js
@@ -0,0 +1,135 @@
+let xhr = new XMLHttpRequest();
+
+xhr.open('POST', 'http://10.3.4.224:8080/', true);
+
+let request_types = {
+    request: "get_types_documents"
+}
+
+xhr.send(JSON.stringify(request_types));
+
+xhr.onload = function() {
+    let responseObj = xhr.response;
+    console.log(JSON.parse(responseObj))
+
+    responseObj = JSON.parse(responseObj)
+
+    for (let type_config of responseObj.types) {
+
+        let type_name = type_config.name;
+
+        let content = document.querySelector(".content");
+    
+        let element = document.createElement('div');
+        element.classList.add("file-type");
+        element.innerHTML = `
+        <div class="file-type-name">${type_name}</div>
+        <div class="file-type-bn">
+            <svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 14L0.20577 0.5L15.7942 0.500002L8 14Z"/>
+            </svg>
+        </div>
+        <div class="white-block"></div>
+        `
+        
+        content.append(element);
+
+        let elementTable = document.createElement('table');
+        elementTable.innerHTML = `
+        <tr class="sort">
+            <td class="sort-cat sort-filenames"><div class="sort-content">
+                <div class="text">Имя</div>
+                <div class="sort-bn">
+                    <svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8 14L0.20577 0.5L15.7942 0.500002L8 14Z"/>
+                    </svg>
+                </div>
+            </div></td>
+            <td class="sort-cat sort-author"><div class="sort-content">
+                <div class="text">Автор</div>
+                <div class="sort-bn">
+                    <svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8 14L0.20577 0.5L15.7942 0.500002L8 14Z"/>
+                    </svg>
+                </div>
+            </div></td>
+            <td class="sort-cat sort-editdate"><div class="sort-content">
+                <div class="text">Дата изменения</div>
+                <div class="sort-bn">
+                    <svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8 14L0.20577 0.5L15.7942 0.500002L8 14Z"/>
+                    </svg>
+                </div>
+            </div></td>
+            <td class="sort-cat"></td>
+        </tr>
+        `
+
+        content.append(elementTable);
+        
+
+
+        let xhr2 = new XMLHttpRequest();
+
+        xhr2.open('POST', 'http://10.3.4.224:8080/', true);
+
+        let request_docs = {
+            request: "get_documents_by_type", 
+            type: type_config.id
+        }
+
+        xhr2.send(JSON.stringify(request_docs));
+
+        xhr2.onload = function() {
+            
+            let responseObjdocs = xhr2.response;
+            console.log(JSON.parse(responseObjdocs))
+
+            responseObjdocs = JSON.parse(responseObjdocs)
+
+            for (let doc of responseObjdocs.documents) {
+
+                let doc_name = doc.name
+                let doc_author = doc.author
+                let doc_was_updated = doc.was_updated
+                var date = new Date(doc_was_updated);
+                doc_was_updated = ('0' + date.getDate()).slice(-2) + '.' + ('0' + (date.getMonth() + 1)).slice(-2) + '.' + date.getFullYear() + ' ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2);
+
+                let table = document.querySelectorAll("table")[document.querySelectorAll("table").length - 1];
+            
+                let elementTr = document.createElement('tr');
+                elementTr.innerHTML = `
+                <td>
+                   <div class="name_file">
+                       ${doc_name}
+                   </div>
+                </td>
+                <td>
+                    <div class="name_author_1">
+                        ${doc_author}
+                    </div>
+                </td>
+                <td>
+                    <div class="data_1">
+                        ${doc_was_updated}
+                    </div>
+                </td>
+                <td>
+                    <div class="input__wrapper">
+
+                        <a class="bn31 open-file" href="#">
+                            <span class="bn31span">Открыть документ</span>
+                        </a>
+                    </div>
+                </td>
+                `
+                
+                table.append(elementTr);
+            }
+        }
+
+    }
+
+    page_init()
+
+};
--- script.js
+++ script.js
@@ -1,49 +1,20 @@
-let fields = document.querySelectorAll('.field__file');
-Array.prototype.forEach.call(fields, function (input) {
-    let label = input.nextElementSibling,
-        labelVal = label.querySelector('.field__file-fake').innerText;
+function page_init() {
 
-    input.addEventListener('change', function (e) {
-        let countFiles = '';
-        if (this.files && this.files.length >= 1)
-            countFiles = this.files.length;
+    let switch_list = document.querySelectorAll(".file-type");
+    let switch_icon = document.querySelectorAll(".file-type-bn");
+    let table_list = document.querySelectorAll("table");
 
-        if (countFiles)
-            label.querySelector('.field__file-fake').innerText = 'Выбрано файлов: ' + countFiles;
-        else
-            label.querySelector('.field__file-fake').innerText = labelVal;
-    });
-});
+    function switch_table(btn, i) {
 
-
-function DropDown(el) {
-    this.dd = el;
-    this.placeholder = this.dd.children('span');
-    this.opts = this.dd.find('ul.dropdown > li');
-    this.val = '';
-    this.index = -1;
-    this.initEvents();
-}
-DropDown.prototype = {
-    initEvents : function() {
-        var obj = this;
-
-        obj.dd.on('click', function(event){
-            $(this).toggleClass('active');
-            return false;
-        });
-
-        obj.opts.on('click',function(){
-            var opt = $(this);
-            obj.val = opt.text();
-            obj.index = opt.index();
-            obj.placeholder.text(obj.val);
-        });
-    },
-    getValue : function() {
-        return this.val;
-    },
-    getIndex : function() {
-        return this.index;
+        btn.onclick = function() {
+            switch_icon[i].classList.toggle("rotated");
+            table_list[i].classList.toggle("visible");
+        }
     }
+
+    for (let i = 0; i < switch_list.length; i++) {
+        
+        switch_table(switch_list[i], i); 
+    }
+    
 }
